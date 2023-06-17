bf98f270c728a4a7852cd1bf80de9b3e3e897e80
ldeeley <lester.deeley@yahoo.com>
2023-06-17 14:47:35 +0100

    Initial commit

---
+++ src/main/java/com/example/rnralbumart/RnRAlbumArtApplication.java
@@ -0,0 +1,13 @@
+package com.example.rnralbumart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnRAlbumArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RnRAlbumArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rnralbumart/RnRAlbumArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rnralbumart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnRAlbumArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
